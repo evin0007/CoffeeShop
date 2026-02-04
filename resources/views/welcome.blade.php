6f12db57f015516321780be79907809c7d372462
evin0007 <shervinjimenez53@gmail.com>
2026-02-04 22:19:35 +0800

    Correct vite path in welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Coffee Shop</title>
     
-    @vite(['resources/js/app.jsx'])
-
-@vite(['react/src/main.jsx'])
+    {{-- Ito dapat ang nakasulat base sa manifest mo --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app"></div>
